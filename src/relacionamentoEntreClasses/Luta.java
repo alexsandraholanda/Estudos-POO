b2f52a3fb7311b8e5d7d28dbabb9ce03221d1500
alexsandraholanda <alexsandraale8795@gmail.com>
2020-04-07 01:12:26 -0300

    Agregação (parte 1 )

---
+++ src/relacionamentoEntreClasses/Luta.java
@@ -0,0 +1,51 @@
+package relacionamentoEntreClasses;
+
+public class Luta {
+	private Lutador desafiado;
+	private Lutador desafiante;
+	private int raunds;
+	private boolean aprovada;
+	
+	
+	public void marcarLuta() {
+		
+	}
+	
+	public void lutar() {
+		
+	}
+
+	public Lutador getDesafiado() {
+		return desafiado;
+	}
+
+	public void setDesafiado(Lutador desafiado) {
+		this.desafiado = desafiado;
+	}
+
+	public Lutador getDesafiante() {
+		return desafiante;
+	}
+
+	public void setDesafiante(Lutador desafiante) {
+		this.desafiante = desafiante;
+	}
+
+	public int getRaunds() {
+		return raunds;
+	}
+
+	public void setRaunds(int raunds) {
+		this.raunds = raunds;
+	}
+
+	public boolean isAprovada() {
+		return aprovada;
+	}
+
+	public void setAprovada(boolean aprovada) {
+		this.aprovada = aprovada;
+	}
+	
+	
+}
